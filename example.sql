9f374b12bc40852d5413a7d6ae0682673763c8c9
DUVER ENRIQUE GUTIERREZ AVILA <duver.gutierrez@correo.policia.gov.co>
2020-12-11 20:08:21 -0500

    Actualizacion Base de Datos

--- example.sql
+++ example.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-12-2020 a las 20:28:24
--- Versión del servidor: 10.4.14-MariaDB
--- Versión de PHP: 7.4.10
+-- Tiempo de generación: 12-12-2020 a las 02:06:10
+-- Versión del servidor: 10.4.17-MariaDB
+-- Versión de PHP: 8.0.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,6 +33,28 @@ CREATE TABLE `categories` (
   `status_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `status_id`) VALUES
+(1, 'terror', 1),
+(2, 'comedia', 1),
+(7, 'Suspenso', 1),
+(8, 'Infantil', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `category_movie`
+--
+
+CREATE TABLE `category_movie` (
+  `id` int(11) NOT NULL,
+  `movie_id` int(11) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 -- --------------------------------------------------------
 
 --
@@ -46,6 +68,15 @@ CREATE TABLE `movies` (
   `user_id` int(11) DEFAULT NULL,
   `status_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `movies`
+--
+
+INSERT INTO `movies` (`id`, `name`, `description`, `user_id`, `status_id`) VALUES
+(2, 'Blancanieves', 'pelicula Infantil', 3, 1),
+(3, 'Blancanieves', 'pelicula Infantil', 3, 1),
+(8, 'Caperucita', 'pelicula Infantil', 4, 1);
 
 -- --------------------------------------------------------
 
@@ -75,6 +106,14 @@ CREATE TABLE `rentals` (
   `end_date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `rentals`
+--
+
+INSERT INTO `rentals` (`id`, `start_date`, `total`, `user_id`, `status_id`, `end_date`) VALUES
+(1, '2020-12-09', 1, 1, 1, '2020-12-10'),
+(3, '2020-12-07', 3, 3, 1, '2020-12-24');
+
 -- --------------------------------------------------------
 
 --
@@ -93,7 +132,8 @@ CREATE TABLE `roles` (
 
 INSERT INTO `roles` (`id`, `name`, `status_id`) VALUES
 (1, 'administrador', 1),
-(2, 'usuario', 1);
+(2, 'usuario', 1),
+(7, 'Basico', 1);
 
 -- --------------------------------------------------------
 
@@ -107,6 +147,14 @@ CREATE TABLE `statuses` (
   `type_status_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `statuses`
+--
+
+INSERT INTO `statuses` (`id`, `name`, `type_status_id`) VALUES
+(1, 'activo', 1),
+(3, 'Inactivo', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -117,6 +165,14 @@ CREATE TABLE `type_statuses` (
   `id` int(11) NOT NULL,
   `name` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `type_statuses`
+--
+
+INSERT INTO `type_statuses` (`id`, `name`) VALUES
+(1, 'disponible'),
+(2, 'no disponible');
 
 -- --------------------------------------------------------
 
@@ -138,7 +194,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `status_id`, `role_id`) VALUES
-(1, 'duver gutierrez', 'duver@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', 1, 1);
+(3, 'andrea ruiz', 'DUVERGUTIERREZ@HOTMAIL.COM', '827ccb0eea8a706c4c34a16891f84e7b', 1, 2),
+(4, 'carlos sanchez', 'carlos@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', 1, 2);
 
 --
 -- Índices para tablas volcadas
@@ -148,38 +205,54 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `status_id`, `role_id`) 
 -- Indices de la tabla `categories`
 --
 ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_STATUSES_CATEGORIES` (`status_id`);
+
+--
+-- Indices de la tabla `category_movie`
+--
+ALTER TABLE `category_movie`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_CATEGORIES_CATEGORYMOVIE` (`category_id`),
+  ADD KEY `FK_MOVIES_CATEGORYMOVIE` (`movie_id`);
 
 --
 -- Indices de la tabla `movies`
 --
 ALTER TABLE `movies`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_USERS_MOVIES` (`user_id`),
+  ADD KEY `FK_STATUSES_MOVIES` (`status_id`);
 
 --
 -- Indices de la tabla `movie_rental`
 --
 ALTER TABLE `movie_rental`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_RENTALS_MOVIERENTAL` (`rental_id`),
+  ADD KEY `FK_MOVIES_MOVIERENTAL` (`movie_id`);
 
 --
 -- Indices de la tabla `rentals`
 --
 ALTER TABLE `rentals`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_STATUSES_RENTALS` (`status_id`);
 
 --
 -- Indices de la tabla `roles`
 --
 ALTER TABLE `roles`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id` (`id`);
+  ADD KEY `id` (`id`),
+  ADD KEY `FK_STATUSES_ROLES` (`status_id`);
 
 --
 -- Indices de la tabla `statuses`
 --
 ALTER TABLE `statuses`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_TYPESTATUSES_STATUSES` (`type_status_id`);
 
 --
 -- Indices de la tabla `type_statuses`
@@ -191,7 +264,9 @@ ALTER TABLE `type_statuses`
 -- Indices de la tabla `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_ROLES_USERS` (`role_id`),
+  ADD KEY `FK_STATUSES_USERS` (`status_id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -201,13 +276,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `categories`
 --
 ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `category_movie`
+--
+ALTER TABLE `category_movie`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `movies`
 --
 ALTER TABLE `movies`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `movie_rental`
@@ -219,31 +300,87 @@ ALTER TABLE `movie_rental`
 -- AUTO_INCREMENT de la tabla `rentals`
 --
 ALTER TABLE `rentals`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `statuses`
 --
 ALTER TABLE `statuses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `type_statuses`
 --
 ALTER TABLE `type_statuses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `categories`
+--
+ALTER TABLE `categories`
+  ADD CONSTRAINT `FK_STATUSES_CATEGORIES` FOREIGN KEY (`status_id`) REFERENCES `statuses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `category_movie`
+--
+ALTER TABLE `category_movie`
+  ADD CONSTRAINT `FK_CATEGORIES_CATEGORYMOVIE` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_MOVIES_CATEGORYMOVIE` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `movies`
+--
+ALTER TABLE `movies`
+  ADD CONSTRAINT `FK_STATUSES_MOVIES` FOREIGN KEY (`status_id`) REFERENCES `statuses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_USERS_MOVIES` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `movie_rental`
+--
+ALTER TABLE `movie_rental`
+  ADD CONSTRAINT `FK_MOVIES_MOVIERENTAL` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_RENTALS_MOVIERENTAL` FOREIGN KEY (`rental_id`) REFERENCES `rentals` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `rentals`
+--
+ALTER TABLE `rentals`
+  ADD CONSTRAINT `FK_STATUSES_RENTALS` FOREIGN KEY (`status_id`) REFERENCES `statuses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD CONSTRAINT `FK_STATUSES_ROLES` FOREIGN KEY (`status_id`) REFERENCES `statuses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `statuses`
+--
+ALTER TABLE `statuses`
+  ADD CONSTRAINT `FK_TYPESTATUSES_STATUSES` FOREIGN KEY (`type_status_id`) REFERENCES `type_statuses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `FK_ROLES_USERS` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_STATUSES_USERS` FOREIGN KEY (`status_id`) REFERENCES `statuses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
